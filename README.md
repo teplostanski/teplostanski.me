558490b600d6ff3c4e796782289e774a9b78d0e1
teplostanski <teplostanski@disroot.org>
2022-09-01 11:22:32 +0000

    feat: create React application

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+My portfolio site built with ReactJS
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,54 @@
+import FooterColumns from '../FooterColumns/FooterColumns';
+import love from '../../assets/images/love.svg';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <div className="footer__col2">
+          <FooterColumns
+            heading="Карта Сайта"
+            links={[
+              {
+                type: 'Link',
+                title: 'Обо мне',
+                path: '/',
+              },
+              {
+                type: 'Link',
+                title: 'Мои проекты',
+                path: '/projects',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col3">
+          <FooterColumns
+            heading="Контакты"
+            links={[
+            ]}
+          />
+        </div>
+        <div className="footer__col4">
+          <FooterColumns
+            heading="Ссылки"
+            links={[
+              {
+                title: 'Github',
+                path: 'https://github.com/r0bomurlok',
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="copyright">
+        <p className="copyright__text">
+          © 2022 - Made with{' '}
+          <img className="copyright__icon" src={love} alt="love" /> by Игорь
+          Теплостанский
+        </p>
+      </div>
+    </div>
+  );
+}
