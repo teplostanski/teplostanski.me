6951fd577105bec6093ef789584463bb8667c3a6
teplostanski <teplostanski@disroot.org>
2025-01-14 03:50:25 +0000

    feat!: completely rewrite internationalization
    BREAKING CHANGE: switched to next-intl with localized routing approach

---
+++ src/shared/constants/links.ts
@@ -0,0 +1,20 @@
+export const links = {
+  developed_by: {
+    label: 'teplostanski',
+    link: 'https://github.com/teplostanski',
+  },
+  moonbloom_theme: {
+    label: '',
+    link: 'https://github.com/moonbloom-theme',
+  },
+  email: {
+    label: 'email',
+    link: 'mailto:teplostanski@disroot.org',
+  },
+  github: { label: 'github', link: 'https://github.com/teplostanski' },
+  telegram: { label: 'telegram', link: 'https://t.me/teplostanski' },
+  bsky: {
+    label: 'bsky',
+    link: 'https://bsky.app/profile/teplostanski.bsky.social',
+  },
+} as const
---
+++ src/shared/hooks/useLocaleMessages.ts
@@ -0,0 +1,81 @@
+'use client'
+
+import { useCurrentLocale } from '@/shared/hooks/useCurrentLocale'
+
+const messagesRu = {
+  Header: {
+    title: 'игорь теплостанский',
+    links: {
+      back: 'назад',
+      home: 'на главную',
+      projects: 'проекты',
+    },
+  },
+  Home: {
+    disclaimer: 'Всё в порядке! Css стили подгрузились ;3',
+    about: {
+      messages: {
+        first:
+          'Всем привет 👋 Меня зовут Игорь, я frontend developer. В свободное время разрабатываю <vite_plugin>vite-plugin-pretty-module-classnames</vite_plugin> и <moonbloom_theme>Moonbloom Theme</moonbloom_theme>.',
+        second:
+          'Мне нравится создавать приложения и проекты с открытым исходным кодом. Ниже я оставлю все необходимые ссылочки 👇',
+      },
+    },
+  },
+
+  ProjectsPage: {
+    title: 'проекты',
+    description:
+      'Здесь собраны мои пет-проекты и эксперименты, не ограниченные только фронтендом',
+  },
+
+  Common: {
+    language: 'Язык',
+    links: 'ссылки',
+    more: 'подробнее...',
+  },
+}
+
+const messagesEn = {
+  Header: {
+    title: 'igor teplostanski',
+    links: {
+      back: 'back',
+      home: 'home',
+      projects: 'projects',
+    },
+  },
+  Home: {
+    disclaimer: 'All good! CSS styles have loaded successfully ;3',
+    about: {
+      messages: {
+        first:
+          'Hi everyone 👋 My name is Igor, and I’m a frontend developer. In my free time, I develop <vite_plugin>vite-plugin-pretty-module-classnames</vite_plugin>, and work on <moonbloom_theme>Moonbloom Theme</moonbloom_theme>.',
+        second:
+          'I enjoy creating applications and open-source projects. Below, I’ve left all the necessary links for you 👇',
+      },
+    },
+  },
+
+  ProjectsPage: {
+    title: 'projects',
+    description:
+      'A collection of my side projects and experiments beyond just frontend',
+  },
+
+  Common: {
+    language: 'Language',
+    links: 'links',
+    more: 'more...',
+  },
+}
+
+const MESSAGE_MAP = {
+  en: messagesEn,
+  ru: messagesRu,
+} as const
+
+export const useLocaleMessages = () => {
+  const { locale } = useCurrentLocale()
+  return MESSAGE_MAP[locale]
+}
