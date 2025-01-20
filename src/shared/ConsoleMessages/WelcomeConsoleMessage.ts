a1b7aeae07c4b9e796ff995b637af3c69bec3d37
teplostanski <teplostanski@disroot.org>
2025-01-20 22:53:48 +0000

    feat: add localization for console messages

--- src/shared/ConsoleMessages/WelcomeConsoleMessage.ts
+++ src/shared/ConsoleMessages/WelcomeConsoleMessage.ts
@@ -1,46 +1,30 @@
 'use client'
 
-import { title } from 'process'
-// import { useTranslations } from 'use-intl'
 import { useEffect } from 'react'
+import { useTranslations } from 'use-intl'
+import { links } from '../constants/links'
+import { CONSOLE_STYLES } from '../config/constants/constants'
 
 export const WelcomeConsoleMessage = () => {
-  // const t = useTranslations('Messages.console.welcome')
-
-  const TITLE_STYLE = 'font-size: 20px; font-weight: bold;'
-  const SUBTITLE_STYLE = 'font-size: 16px; font-weight: bold;'
-  const TEXT_STYLE = 'font-size: 13px;'
-
-  const title = 'Привет, мир!'
-  const description = 'Ценю твоё любопытство! 🌟'
-  const contribute =
-    'Если тобой были найдены баги, пожалуйста, создай issue или напиши мне на почту.'
-  const donate =
-    'Если есть возможность поддержать финансово, то это будет здорово! Ссылочка внизу 👇'
-  const issueTitle = 'Issue'
-  const issueLink = 'https://github.com/teplostanski/teplostanski.dev/issues'
-  const emailTitle = 'Почта'
-  const emailLink = 'teplostanski@disroot.org'
-  const donateTitle = 'Donate'
-  const donateLink = 'https://donate.teplostanski.dev'
-  const happyHacking = 'Happy hacking! 🚀'
+  const t = useTranslations('Console.welcome')
+  const { issue, email, donate } = links
 
   useEffect(() => {
     console.log(
-      `%c${title}\n\n%c${description}\n\n%c${contribute}\n%c${donate}\n\n%c${issueTitle}\n%c${issueLink}\n\n%c${emailTitle}\n%c${emailLink}\n\n%c${donateTitle}\n%c${donateLink}\n\n%c${happyHacking}`,
-      TITLE_STYLE,
-      TEXT_STYLE,
-      TEXT_STYLE,
-      TEXT_STYLE,
-      SUBTITLE_STYLE,
-      TEXT_STYLE,
-      SUBTITLE_STYLE,
-      TEXT_STYLE,
-      SUBTITLE_STYLE,
-      TEXT_STYLE,
-      SUBTITLE_STYLE,
+      `%c${t('title')}\n\n%c${t('description')}\n\n%c${t('contribute')}\n%c${t('donate')}\n\n%c${t('issueTitle')}\n%c${issue.link}\n\n%c${t('emailTitle')}\n%c${email.link.replace('mailto:', '')}\n\n%c${t('donateTitle')}\n%c${donate.link}\n\n%c${t('happyHacking')}`,
+      CONSOLE_STYLES.title,
+      CONSOLE_STYLES.text,
+      CONSOLE_STYLES.text,
+      CONSOLE_STYLES.text,
+      CONSOLE_STYLES.subtitle,
+      CONSOLE_STYLES.text,
+      CONSOLE_STYLES.subtitle,
+      CONSOLE_STYLES.text,
+      CONSOLE_STYLES.subtitle,
+      CONSOLE_STYLES.text,
+      CONSOLE_STYLES.subtitle,
     )
-  }, [])
+  }, [t])
 
   return null
 }
