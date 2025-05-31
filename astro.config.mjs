bc6611cdfdbe3801925305993515f93bc83d1fe9
teplostanski <teplostanski@disroot.org>
2025-05-31 00:03:06 +0000

    Add vite-plugin-readable-classnames

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import readableClassnames from 'vite-plugin-readable-classnames'
 
 export default defineConfig({
   site: 'https://teplostan.ski',
   integrations: [sitemap()],
-});
+  vite: { plugins: [readableClassnames({lineNumber: true})] },
+})
