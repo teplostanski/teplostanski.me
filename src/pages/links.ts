6afe80fb36cc4a785633979198d17663ce0de4e3
teplostanski <teplostanski@disroot.org>
2025-08-11 18:16:59 +0000

    add new links

--- src/pages/links.ts
+++ src/pages/links.ts
@@ -20,9 +20,14 @@ export const links = {
     link: 'mailto:teplostanski@disroot.org',
   },
   github: { label: 'github', link: 'https://github.com/teplostanski' },
+  codeberg: { label: 'codeberg', link: 'https://codeberg.org/teplostanski' },
   telegram: { label: 'telegram', link: 'https://t.me/teplostanski' },
   bsky: {
     label: 'bsky',
     link: 'https://bsky.app/profile/teplostan.ski',
+  },
+  keyoxide: {
+    label: 'keyoxide',
+    link: 'https://keyoxide.org/hkp/C4339B5E84224FB8B77DD3D5F0ED43D371910F09',
   },
 } as const
