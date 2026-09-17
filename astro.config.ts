be03ba5209373586c23b37aeb99189fa83bc9651
teplostanski <contact@teplostanski.me>
2026-09-17 00:09:12 +0000

    fix: restore goatCounterIntegration

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,13 @@ export default defineConfig({
   integrations: [
     UnoCSS(),
     sitemap(),
+    goatCounterIntegration({
+      endpoint: 'https://stats.teplostanski.me/count',
+      allowLocal: true, // для dev-режима
+      params: {
+        path: (p) => location.host + p,
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [remarkGithubBlockquoteAlert],
